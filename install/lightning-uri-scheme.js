73227e00e17db09fb90a113bb7af30813a78279f
AMF <austin.fay@alltechnologysolutions.com>
2019-01-09 03:04:35 -0700

    laid groundwork for cross platform lightning uri support via default browser

---
+++ install/lightning-uri-scheme.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log("TODO: lighting uri scheme installation");
+//TODO:
+//Detect OS
+
+//find default browser path
+
+//Run lightning-uri-scheme.reg // rebuild it to pass browser path if on windows
+
+//create lightning.desktop in /usr/share/application/ on ubuntu https://specifications.freedesktop.org/desktop-entry-spec/latest/index.html
+//run sudo update-desktop-database
