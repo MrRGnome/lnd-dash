25981e07565d1fb0d87505dcbe9651aea0e723e5
AMF <austin.fay@alltechnologysolutions.com>
2019-02-10 13:48:45 -0700

    fixed possible event emitter memory leak and enhanced socket closing managment.

--- sockroutes/wss.js
+++ sockroutes/wss.js
@@ -4,32 +4,45 @@ var lndState = require('./lndstate');
 var sockAuth = require('./sockauth');
 var WebSocket = require('ws'); 
 
+var clients = [];
 
 module.exports = (server) => {
     var wss = new WebSocket.Server({ server });
-    
+
+    notifications.on('notification', (event, data) => {
+        for (var i = 0; i < clients.length; i++) {
+            var ws = clients[i].ws;
+            var req = clients[i].req;
+
+            sockAuth(ws, req);
+            if (ws.readyState == ws.OPEN) {
+                ws.send(JSON.stringify({ "event": event, "data": data }));
+            }
+        }
+    });
+
+    lndState.notifications.on('notification', (event, data) => {
+        for (var i = 0; i < clients.length; i++) {
+            var ws = clients[i].ws;
+            var req = clients[i].req;
+
+            sockAuth(ws, req);
+            if (ws.readyState == ws.OPEN) {
+                ws.send(JSON.stringify({ "event": event, "data": data }));
+            }
+        }
+    });
 
     wss.on('connection', (ws, req) => {
+        clients.push({ ws: ws, req: req });
 
         sockAuth(ws, req);
         if (ws.readyState == ws.OPEN) 
             ws.send(JSON.stringify({ "event": "lndState", "data": lndState.lndState }));
 
-        notifications.on('notification', (event, data) => {
-            sockAuth(ws, req);
-            if (ws.readyState == ws.OPEN) {
-                ws.send(JSON.stringify({ "event": event, "data": data }));
-            }
+        ws.on('close', (sock) => {
+            clients = clients.filter(client => client.ws != ws);
         });
-
-        lndState.notifications.on('notification', (event, data) => {
-            sockAuth(ws, req);
-            if (ws.readyState == ws.OPEN) {
-                ws.send(JSON.stringify({ "event": event, "data": data }));
-            }
-        });
-
-
     });
 
     return wss;
