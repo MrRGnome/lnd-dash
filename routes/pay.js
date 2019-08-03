10f0babef4c341a616be52f8864bf29d2dd58337
AMF <austin.fay@alltechnologysolutions.com>
2019-08-03 17:26:33 -0600

    polluting master with a million minor changes for testing because it's more convenient than branching. you're welcome.

--- routes/pay.js
+++ routes/pay.js
@@ -17,7 +17,7 @@ router.post('/decode_request', async function (req, res) {
 
 router.post('/', async function (req, res) {
     if (alreadyPaid[req.body.payment_request])
-        res.status(200).json({ status: "fail", data: {error_message: "Already paid invoice this session"}});
+        return res.status(200).json({ status: "fail", data: {error_message: "Already paid invoice this session"}});
     var result = await lightningService.sendPaymentSync(req.body.payment_request, res.locals.user);
     if (result.status == "success")
         alreadyPaid[req.body.payment_request] = true;
@@ -25,8 +25,8 @@ router.post('/', async function (req, res) {
 });
 
 router.post('/btc', async function (req, res) {
-    if (!req.body.transaction || !req.body.transaction.address || !req.body.transaction.amount)
-        res.status(200).json({ status: "fail", data: { error_message: "No transaction data" } });
+    if (!req.body.transaction || !req.body.transaction.addr || !req.body.transaction.amount)
+        return res.status(200).json({ status: "fail", data: { error_message: "No transaction data" } });
     console.log(req.body.transaction);
     try {
         var result = await lightningService.sendCoins(req.body.transaction, res.locals.user);
