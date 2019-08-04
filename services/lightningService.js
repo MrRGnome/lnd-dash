f0b960ea83715ba88324b24f9efd833e20665b47
AMF <austin.fay@alltechnologysolutions.com>
2019-08-03 18:33:46 -0600

    this could be the big one

--- services/lightningService.js
+++ services/lightningService.js
@@ -840,7 +840,7 @@ module.exports = {
         var timeout = new Date().setSeconds(new Date().getSeconds() + timeout_in_seconds);
 
         var call = new Promise((resolve) => {
-            client.data.client.sendCoins({ addr: transaction.addr, amount: parseInt(transaction.amount), target_conf: transaction.target_conf ? parseInt(transaction.target_conf) : undefined, sat_per_byte: transaction.sat_per_byte ? parseInt(sat_per_byte) : undefined, send_all: transaction.send_all == 'true' ? true : undefined }, function (err, response) {
+            client.data.client.sendCoins({ addr: transaction.addr, amount: parseInt(transaction.amount), target_conf: transaction.target_conf ? parseInt(transaction.target_conf) : undefined, sat_per_byte: transaction.sat_per_byte ? parseInt(sat_per_byte) : undefined, send_all: transaction.send_all == 'true' ? true : undefined }, client.data.metadata, { deadline: timeout }, function (err, response) {
                 console.log("postcall");
                 console.log(err);
                 console.log(response);
